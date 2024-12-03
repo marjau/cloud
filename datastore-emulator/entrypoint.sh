811e0e7d445ad87ac10cd6fb460261581a8e36f7
Marcos Jauregui <it.marcosj@gmail.com>
2024-12-02 21:55:41 -0600

    add datastore-emulator

---
+++ datastore-emulator/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+export PATH=$PATH:${GCLOUD_DIR}/bin
+
+PERSIST_DATA_FLAG="--no-store-on-disk"
+if [ "$GCLOUD_STORE_ON_DISK" == true ]
+then
+    PERSIST_DATA_FLAG="--store-on-disk"    
+fi
+
+gcloud beta emulators datastore start \
+    --project=$CLOUDSDK_CORE_PROJECT \
+    --use-firestore-in-datastore-mode \
+    --host-port=$DS_HOST:8081 \
+    $PERSIST_DATA_FLAG
